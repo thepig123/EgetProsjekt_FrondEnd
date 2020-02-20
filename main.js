6a4c18a14cdbf3ac84ab59735534f90f31e181c6
bolle saus <bollesaus@bolles-MacBook-Air.local>
2020-02-20 10:01:07 +0100

    div

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function setDark() {
 	document.documentElement.style.setProperty("--title", "#ffffff");
 	document.documentElement.style.setProperty("--content", "#d7d7d7");
 	document.documentElement.style.setProperty("--background", "#151515");
+	document.documentElement.style.setProperty("--shadow", "255, 255, 255, 0.1");
 }
 
 // Light mode
@@ -15,6 +16,7 @@ function setLight() {
 	document.documentElement.style.setProperty("--title", "#111111");
 	document.documentElement.style.setProperty("--content", "#000000");
 	document.documentElement.style.setProperty("--background", "#ffffff");
+	document.documentElement.style.setProperty("--shadow", "0, 0, 0, 0.1");
 }
 
 
